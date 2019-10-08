d9dbe55336037adbf6ec06061cf169d057650a50
ad19900913 <ad19900913@outlook.com>
2019-10-09 07:46:08 +0800

    选数

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,7 @@ public class Main {
             array[i] = num;
         }
         //选数
-        int total = 0;
-        for (int i = 0; i <= n - k; i++) {
-            total += selectNumber(k, 0, i, n - 1);
-        }
-        System.out.println(total);
+        System.out.println(selectNumber(k, 0, 0, n - 1));
     }
 
     /**
@@ -50,7 +46,7 @@ public class Main {
      */
     private static boolean isPrime(int n) {
         int max = (int) Math.sqrt(n);
-        for (int i = 2; i < max; i++) {
+        for (int i = 2; i <= max; i++) {
             if (n % i == 0) {
                 return false;
             }
--- src/入门/选数.java
+++ src/入门/选数.java
@@ -1,4 +1,59 @@
 package 入门;
 
+import java.util.Scanner;
+
 public class 选数 {
+    private static int[] array;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        array = new int[n];
+        int k = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            int num = scanner.nextInt();
+            array[i] = num;
+        }
+        //选数
+        System.out.println(selectNumber(k, 0, 0, n - 1));
+    }
+
+    /**
+     * 选数
+     *
+     * @param remain 还剩几个数没选
+     * @param sum    当前选的所有数之和
+     * @param start  可以选择的数字起始下标
+     * @return 质数返回1，否则返回0
+     */
+    private static int selectNumber(int remain, int sum, int start, int end) {
+        if (remain == 0) {
+            if (isPrime(sum)) {
+                System.out.println(sum);
+                return 1;
+            }
+            return 0;
+        }
+        int result = 0;
+        for (int i = start; i <= end; i++) {
+            result += selectNumber(remain - 1, sum + array[i], i + 1, end);
+        }
+        return result;
+    }
+
+    /**
+     * 判断某数是否为素数
+     *
+     * @param n
+     * @return
+     */
+    private static boolean isPrime(int n) {
+        int max = (int) Math.sqrt(n);
+        for (int i = 2; i <= max; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
