6ff93eb7ddd30da08f3f80aafc05c6e1231f9a7a
ad19900913 <ad19900913@outlook.com>
2019-11-06 18:47:50 +0800

    玩具谜题

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,60 @@ import java.util.*;
 
 public class Main {
 
+    private static class Node {
+        private int flag;
+        private String name;
+
+        public Node(int flag, String name) {
+            this.flag = flag;
+            this.name = name;
+        }
+
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
         int m = scanner.nextInt();
+        scanner.nextLine();
+        Node [] nodes = new Node[n];
+        for (int i = 0; i < n; i++) {
+            String line = scanner.nextLine();
+            String[] strings = line.split(" ");
+            nodes[i] = new Node(Integer.parseInt(strings[0]), strings[1]);
+        }
+        int index = 0;
+        for (int i = 0; i < m; i++) {
+            int direction = scanner.nextInt();
+            int count = scanner.nextInt();
+            count %= n;
+            if (direction == 0) {//左
+                if (nodes[index].flag == 0) {//朝内
+                    index-=count;
+                    if (index < 0) {
+                        index += n;
+                    }
+                } else {//朝外
+                    index+=count;
+                    if (index >= n) {
+                        index %= n;
+                    }
+                }
+            } else {//右
+                if (nodes[index].flag == 0) {//朝内
+                    index+=count;
+                    if (index >= n) {
+                        index %= n;
+                    }
+                } else {//朝外
+                    index-=count;
+                    if (index < 0) {
+                        index += n;
+                    }
+                }
+            }
+        }
+        System.out.println(nodes[index].name);
     }
 
 }
